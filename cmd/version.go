2672d27b9718dfde67486dff9131d019d046d091
sosozhuang <sosozhuang@163.com>
2017-05-08 19:28:19 +0800

    add version.go

---
+++ cmd/version.go
@@ -0,0 +1,47 @@
+// Copyright © 2017 sosozhuang <sosozhuang@163.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"runtime"
+)
+
+// versionCmd represents the version command that show the version of paxos
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "show the version of paxos",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: printVersion,
+}
+
+const version = "0.1.0"
+
+func printVersion(cmd *cobra.Command, args []string) {
+	fmt.Printf("paxos Version: %s\n", version)
+	fmt.Printf("Go Version: %s\n", runtime.Version())
+	fmt.Printf("Go OS/Arch: %s/%s\n", runtime.GOOS, runtime.GOARCH)
+}
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+}
