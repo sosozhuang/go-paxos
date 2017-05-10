f02a060354d389ac07f8ad08dc8c1ca66dadea00
sosozhuang <sosozhuang@163.com>
2017-05-10 17:22:42 +0800

    add learnerState

--- paxos/learner.go
+++ paxos/learner.go
@@ -13,8 +13,15 @@
 // limitations under the License.
 package paxos
 
-type Learner interface {
+import (
+	"github.com/sosozhuang/paxos/store"
+	"github.com/sosozhuang/paxos/comm"
+	"hash/crc32"
+	"github.com/gogo/protobuf/proto"
+)
 
+type Learner interface {
+	isIMLast() bool
 }
 
 func NewLearner() (Learner, error) {
@@ -26,5 +33,41 @@ type learner struct {
 }
 
 type learnerState struct {
+	value []byte
+	learned bool
+	checksum uint32
+	storage store.MultiGroupStorage
+}
 
+func (l *learnerState) learnWithoutWrite(instanceID comm.InstanceID, value []byte, checksum uint32) {
+	l.value = value
+	l.learned = true
+	l.checksum = checksum
+}
+
+func (l *learnerState) learn(instanceID comm.InstanceID, b ballot, value []byte, checksum uint32) error {
+	if instanceID > 0 && checksum == 0 {
+		l.checksum = 0
+	} else if len(l.value) > 0 {
+		l.checksum = crc32.Update(checksum, crcTable, l.value)
+	}
+	state := &comm.AcceptorStateData{
+		InstanceID: proto.Uint64(instanceID),
+		AcceptedValue: l.value,
+		PromiseID: proto.Uint64(b.proposalID),
+		PromiseNodeID: proto.Uint64(b.nodeID),
+		AcceptedID: proto.Uint64(b.proposalID),
+		AcceptedNodeID: proto.Uint64(b.nodeID),
+		Checksum: proto.Uint32(l.checksum),
+	}
+	b, err := proto.Marshal(state)
+	if err != nil {
+		return err
+	}
+	if err := l.storage.Set(0, instanceID, b); err != nil {
+		return err
+	}
+
+	l.learnWithoutWrite(instanceID, value, l.checksum)
+	return nil
 }
