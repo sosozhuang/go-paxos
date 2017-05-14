0490dc9c0482d5482a50783b071f27acdb7011e1
sosozhuang <sosozhuang@163.com>
2017-05-14 22:34:32 +0800

    Add Learner methods and learner fields

--- paxos/learner.go
+++ paxos/learner.go
@@ -14,14 +14,19 @@
 package paxos
 
 import (
-	"github.com/sosozhuang/paxos/store"
-	"github.com/sosozhuang/paxos/comm"
-	"hash/crc32"
 	"github.com/gogo/protobuf/proto"
+	"github.com/sosozhuang/paxos/checkpoint"
+	"github.com/sosozhuang/paxos/comm"
+	"github.com/sosozhuang/paxos/store"
+	"hash/crc32"
 )
 
 type Learner interface {
+	start()
+	Stop()
+	SetInstanceID(comm.InstanceID)
 	newInstance()
+	AskForLearn()
 	onAskForLearn(*comm.PaxosMsg)
 	onConfirmAskForLearn(*comm.PaxosMsg)
 	onProposalFinished(*comm.PaxosMsg)
@@ -31,23 +36,33 @@ type Learner interface {
 	onAskForCheckpoint(*comm.PaxosMsg)
 	proposalFinished(comm.InstanceID, proposalID)
 	isLearned() bool
-	isIMLast() bool
+	isReadyForNewValue() bool
 }
 
-func NewLearner() (Learner, error) {
+func newLearner(nodeID comm.NodeID, instance Instance, tp Transporter, st store.Storage, acceptor Acceptor, cpm checkpoint.CheckpointManager) (Learner, error) {
 	return nil, nil
 }
 
 type learner struct {
-	state *learnerState
-	acceptor Acceptor
-	learning bool
-	nodeID comm.NodeID
-	instanceID comm.InstanceID
-	lastAckInstanceID comm.InstanceID
+	state              learnerState
+	acceptor           Acceptor
+	learning           bool
+	nodeID             comm.NodeID
+	instanceID         comm.InstanceID
+	lastAckInstanceID  comm.InstanceID
 	lastSeenInstanceID comm.InstanceID
-	lastSeenNodeID comm.NodeID
-	tp Transporter
+	lastSeenNodeID     comm.NodeID
+	cpm                checkpoint.CheckpointManager
+	tp                 Transporter
+	sender
+}
+
+func (l *learner) start() {
+	l.sender.start()
+}
+
+func (l *learner) SetInstanceID(id comm.InstanceID) {
+	l.instanceID = id
 }
 
 func (l *learner) SetLastSeenInstance(instanceID comm.InstanceID, nodeID comm.NodeID) {
@@ -60,20 +75,32 @@ func (l *learner) SetLastSeenInstance(instanceID comm.InstanceID, nodeID comm.No
 
 func (l *learner) proposalFinished(instanceID comm.InstanceID, proposalID proposalID) {
 	msg := &comm.PaxosMsg{
-		Type: comm.PaxosMsgType_ProposalFinished.Enum(),
-		InstanceID: proto.Uint64(instanceID),
-		NodeID: proto.Uint64(l.nodeID),
-		ProposalID: proto.Uint64(proposalID),
+		Type:         comm.PaxosMsgType_ProposalFinished.Enum(),
+		InstanceID:   proto.Uint64(instanceID),
+		NodeID:       proto.Uint64(l.nodeID),
+		ProposalID:   proto.Uint64(proposalID),
 		LastChecksum: proto.Uint32(0),
 	}
 	l.tp.Broadcast(msg, localFirst)
 }
 
+func (l *learner) AskForLearn() {
+	go l.askForLearn()
+}
+
+func (l *learner) askForLearn() {
+
+}
+
+func (i *learner) isReadyForNewValue() bool {
+	return i.instanceID + 1 >= i.lastSeenInstanceID
+}
+
 type learnerState struct {
-	value []byte
+	value   []byte
 	learned bool
-	checksum uint32
-	storage store.MultiGroupStorage
+	checksum
+	storage store.Storage
 }
 
 func (l *learnerState) learnWithoutWrite(instanceID comm.InstanceID, value []byte, checksum uint32) {
@@ -89,22 +116,30 @@ func (l *learnerState) learn(instanceID comm.InstanceID, b ballot, value []byte,
 		l.checksum = crc32.Update(checksum, crcTable, l.value)
 	}
 	state := &comm.AcceptorStateData{
-		InstanceID: proto.Uint64(instanceID),
-		AcceptedValue: l.value,
-		PromiseID: proto.Uint64(b.proposalID),
-		PromiseNodeID: proto.Uint64(b.nodeID),
-		AcceptedID: proto.Uint64(b.proposalID),
+		InstanceID:     proto.Uint64(instanceID),
+		AcceptedValue:  l.value,
+		PromisedID:     proto.Uint64(b.proposalID),
+		PromisedNodeID: proto.Uint64(b.nodeID),
+		AcceptedID:     proto.Uint64(b.proposalID),
 		AcceptedNodeID: proto.Uint64(b.nodeID),
-		Checksum: proto.Uint32(l.checksum),
+		Checksum:       proto.Uint32(l.checksum),
 	}
 	b, err := proto.Marshal(state)
 	if err != nil {
 		return err
 	}
-	if err := l.storage.Set(0, instanceID, b); err != nil {
+	if err := l.storage.Set(instanceID, b); err != nil {
 		return err
 	}
 
 	l.learnWithoutWrite(instanceID, value, l.checksum)
 	return nil
+}
+
+type sender struct {
+
+}
+
+func (s *sender) start() {
+
 }
