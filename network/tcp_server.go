b51e13a5981db5bb4d2ffcd2023964b482009cb2
sosozhuang <sosozhuang@163.com>
2017-05-20 21:57:03 +0800

    Add tcp_server.go: receive messages from peers

---
+++ network/tcp_server.go
@@ -0,0 +1,167 @@
+package network
+
+import (
+	"github.com/sosozhuang/paxos/comm"
+	"io"
+	"net"
+	"sync"
+	"time"
+)
+
+type tcpPeerServer struct {
+	//addr        string
+	receiver    comm.Receiver
+	timeout     time.Duration
+	readTimeout time.Duration
+	//cap         int
+	listener *net.TCPListener
+	wg       sync.WaitGroup
+	ch       chan []byte
+	stopped  <-chan struct{}
+}
+
+func newTcpPeerServer(receiver comm.Receiver, addr string, timeout, readTimeout time.Duration, cap int) (*tcpPeerServer, error) {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+	ln, err := net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		return nil, err
+	}
+	return &tcpPeerServer{
+		//addr: addr,
+		receiver:    receiver,
+		timeout:     timeout,
+		readTimeout: readTimeout,
+		listener:    ln,
+		ch:          make(chan []byte, cap),
+	}, nil
+}
+
+func (t *tcpPeerServer) Start(stopped <-chan struct{}) {
+	t.stopped = stopped
+	go t.handleMessage()
+	go t.accept()
+}
+
+func (t *tcpPeerServer) accept() {
+	t.wg.Add(1)
+	defer t.stop()
+	f := func(net.Conn) { t.wg.Done() }
+	delay := time.Millisecond * 5
+	for {
+		select {
+		case <-t.stopped:
+			return
+		default:
+		}
+		t.listener.SetDeadline(time.Now().Add(t.timeout))
+		conn, err := t.listener.Accept()
+		if err != nil {
+			if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
+				continue
+			} else if ok && opErr.Temporary() {
+				if delay < time.Second {
+					delay *= 2
+				}
+				time.Sleep(delay)
+				continue
+			}
+			log.Error("", err)
+		}
+		t.wg.Add(1)
+		sc := newTcpServerConn(conn, t.readTimeout, t.ch, f)
+		//go sc.receiveMessage()
+		go sc.handleRead(t.stopped)
+		delay = time.Millisecond * 5
+	}
+}
+
+func (t *tcpPeerServer) handleMessage() {
+	for msg := range t.ch {
+		t.receiver.ReceiveMessage(msg)
+	}
+}
+
+func (t *tcpPeerServer) stop() {
+	if t.listener != nil {
+		if err := t.listener.Close(); err != nil {
+			log.Error("", err)
+		}
+		t.listener = nil
+	}
+	close(t.ch)
+	t.wg.Done()
+}
+
+func (t *tcpPeerServer) Stop() {
+	t.wg.Wait()
+}
+
+type tcpServerConn struct {
+	conn    net.Conn
+	timeout time.Duration
+	ch      chan<- []byte
+	comm.Node
+	closeFunc
+}
+
+func newTcpServerConn(conn net.Conn, timeout time.Duration, ch chan<- []byte, f closeFunc) *tcpServerConn {
+	return &tcpServerConn{
+		conn:      conn,
+		timeout:   timeout,
+		ch:        ch,
+		closeFunc: f,
+	}
+}
+
+func (t *tcpServerConn) handleRead(stopped <-chan struct{}) {
+	defer t.close()
+	in := make([]byte, 0)
+	b := make([]byte, 1024)
+	for {
+		select {
+		case <-stopped:
+			return
+		default:
+		}
+		t.conn.SetReadDeadline(time.Now().Add(t.timeout))
+		n, err := t.conn.Read(b)
+		if err != nil {
+			if err != io.EOF {
+				if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
+					continue
+				}
+			}
+			if n > 0 {
+				in = unpack(append(in, b[:n]...), t.ch)
+			}
+			log.Error(err)
+			return
+		}
+
+		in = unpack(append(in, b[:n]...), t.ch)
+
+	}
+
+}
+
+//func (t *tcpServerConn) receiveMessage() {
+//	for msg := range t.ch {
+//		t.Node.ReceiveMessage(msg)
+//	}
+//}
+
+func (t *tcpServerConn) close() {
+	if t.conn != nil {
+		if err := t.conn.Close(); err != nil {
+			log.Error(err)
+		}
+		t.conn = nil
+	}
+	//close(t.ch)
+	if t.closeFunc != nil {
+		t.closeFunc(t.conn)
+	}
+}
